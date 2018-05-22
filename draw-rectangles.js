d71beca6a8a74a48941f520448654104594c6312
Melanie <huet.melanie91@gmail.com>
2018-05-22 21:40:18 +0200

    Remove rectangles only when all rotations are done

--- draw-rectangles.js
+++ draw-rectangles.js
@@ -12,13 +12,15 @@ class ColorGenerator {
 }
 
 class Rect {
-  constructor(drawZoneElt, color) {
-    this.drawZoneElt = drawZoneElt;
+  constructor(draw, color) {
+    this.draw = draw;
+
+    this.isRotated = null;
+    this.rotationTime = 2000;
+
     this.elt = document.createElement('div');
     this.elt.style.backgroundColor = color;
     this.elt.style.position = "absolute";
-
-    this.elt.addEventListener("dblclick", this.rotate.bind(this));
   }
 
   insert(originX, originY) {
@@ -27,55 +29,79 @@ class Rect {
     this.elt.style.top = originY + "px";
     this.elt.style.left = originX + "px";
 
-    this.drawZoneElt.appendChild(this.elt);
+    this.draw.drawZoneElt.appendChild(this.elt);
+
+    this.elt.addEventListener("dblclick", this.rotate.bind(this));
   }
 
-  draw(posX, posY) {
+  drawing(posX, posY) {
     this.elt.style.top = Math.min(posY, this.originY) + "px";
     this.elt.style.left = Math.min(posX, this.originX) + "px";
     this.elt.style.height = Math.abs(posY - this.originY) + "px";
     this.elt.style.width = Math.abs(posX - this.originX) + "px";
   }
 
+  validate() {
+    if (!parseInt(this.elt.style.height) || !parseInt(this.elt.style.width)) {
+      this.elt.remove();
+    }
+  }
+
   rotate() {
+    this.isRotated = true;
     this.elt.style.transform = "rotate(360deg)";
-    this.elt.style.transition = "1s ease-in-out";
+    this.elt.style.transition = this.rotationTime + "ms ease-in-out";
 
     var self = this;
-    setTimeout(function() {
-      self.elt.remove();
-    }, 1000);
+    self.delete = setTimeout(function() {
+      self.isRotated = false;
+      let isRotationOngoing = self.draw.getRectangles().find(r => r.isRotated);
+      if (isRotationOngoing) {
+        clearTimeout(self.delete);
+      } else {
+        self.draw.getRectangles().forEach(r => { if (r.isRotated === false) r.elt.remove(); });
+      }
+    }, this.rotationTime);
   }
+
 }
 
 class Draw {
   constructor(drawZoneSelector) {
     this.drawZoneElt = document.querySelector(drawZoneSelector);
 
-    this.bindStartDrawing = this.startDrawing.bind(this);
-    this.bindDrawing = this.drawing.bind(this);
-    this.bindStopDrawing = this.stopDrawing.bind(this);
+    this._rects = [];
 
-    this.drawZoneElt.addEventListener("mousedown", this.bindStartDrawing);
+    this._bindStartDrawing = this.startDrawing.bind(this);
+    this._bindDrawing = this.drawing.bind(this);
+    this._bindStopDrawing = this.stopDrawing.bind(this);
+
+    this.drawZoneElt.addEventListener("mousedown", this._bindStartDrawing);
   }
 
   startDrawing(event) {
     let color = ColorGenerator.random();
-    this.rect = new Rect(this.drawZoneElt, color);
-    this.rect.insert(event.clientX, event.clientY);
+    this.currentRect = new Rect(this, color);
+    this._rects.push(this.currentRect);
+    this.currentRect.insert(event.clientX, event.clientY);
 
-    this.drawZoneElt.addEventListener("mousemove", this.bindDrawing);
-
-    this.drawZoneElt.addEventListener("mouseup", this.bindStopDrawing);
+    this.drawZoneElt.addEventListener("mousemove", this._bindDrawing);
+    this.drawZoneElt.addEventListener("mouseup", this._bindStopDrawing);
   }
 
   drawing(event) {
-    this.rect.draw(event.clientX, event.clientY);
+    this.currentRect.drawing(event.clientX, event.clientY);
   }
 
   stopDrawing(event) {
-    this.drawZoneElt.removeEventListener("mousemove", this.bindDrawing);
-    this.drawZoneElt.removeEventListener("mouseup", this.bindStopDrawing);
+    this.currentRect.validate();
+
+    this.drawZoneElt.removeEventListener("mousemove", this._bindDrawing);
+    this.drawZoneElt.removeEventListener("mouseup", this._bindStopDrawing);
+  }
+
+  getRectangles() {
+    return this._rects;
   }
 }
 
